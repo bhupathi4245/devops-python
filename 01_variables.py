f461c2f74968172471175a6a021fbe38b8a52d8a
Satish <sat116@gmail.com>
2026-03-17 12:36:20 -0400

    examples

--- 01_variables.py
+++ 01_variables.py
@@ -42,19 +42,19 @@ print(f)
 
 # type() function is used to check the data type of a variable
 test_list = [1, 2, 3], ['apple', 'banana', 'cherry']
-print(f"test_list: {test_list} is a {type(test_list)} data type.")
+print(f"test_list: {test_list} is a data type of {type(test_list)}.")
 
 test_tuple = (1, 2, 3), ('apple', 'banana', 'cherry')
-print(f"test_tuple: {test_tuple} is a {type(test_tuple)} data type.")
+print(f"test_tuple: {test_tuple} is a data type of {type(test_tuple)}.")
 
 test_dictionary = {'name': 'Alice', 'age': 30}
-print(f"test_dict: {test_dictionary} is a {type(test_dictionary)} data type.")
+print(f"test_dict: {test_dictionary} is a data type of {type(test_dictionary)}.")
 
 test_set = {1, 2, 3}, {'apple', 'banana', 'cherry'}
-print(f"test_set: {test_set} is a {type(test_set)} data type.")
+print(f"test_set: {test_set} is a data type of {type(test_set)}.")
 
 test_none = None
-print(f"test_none: {test_none} is a {type(test_none)} data type.")
+print(f"test_none: {test_none} is a data type of {type(test_none)}.")
 
 test_complex = 1 + 2j
-print(f"test_complex: {test_complex} is a {type(test_complex)} data type.")
+print(f"test_complex: {test_complex} is a data type of {type(test_complex)}.")
