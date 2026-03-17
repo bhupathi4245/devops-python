9013ac509fd236f11f7e5e2a41b815e392ce7820
Satish <sat116@gmail.com>
2026-03-17 11:32:39 -0400

    somemore examples

--- 01_variables.py
+++ 01_variables.py
@@ -1,9 +1,32 @@
 # This is a single line comment
 
-""" ---
+""" 
     This is a multi-line comment
     that spans multiple lines
---- """
+ """
 
 a = 42
 print(a)
+
+"""
+- Boolean values: True, False
+- Strings: "Hello", 'World'
+"""
+
+# Floating-point numbers: 3.14, -0.001
+b = 3.14
+print(b)
+
+# boolean value.. Pythons is case senstive True and true are different
+c = True
+print(c)
+
+# Strings
+d = "Hello, World!"
+e = 'Python is great!'
+print(d)
+print(e)
+
+# Todays weather is nice, but tomorrow it will be rainy.
+f = 'Todays weather is nice, but tomorrow it will be rainy.'
+print(f)
