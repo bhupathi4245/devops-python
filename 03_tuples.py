7ec484d5d98b7581af02ae08687cb389ad8f14d0
Satish <sat116@gmail.com>
2026-03-17 16:50:35 -0400

    unpack method

--- 03_tuples.py
+++ 03_tuples.py
@@ -27,4 +27,22 @@ print(f"Concatenated tuple: {res_tuple}")
 
 res_tuple = sample_tuple * 2  # Repetition
 print(f"Repeated tuple: {res_tuple}")
+
+# Methods
+k8s_idx = sample_tuple.index("K8s")  # 4
+print(f"Index of 'K8s': {k8s_idx}")
+count_ansible = sample_tuple.count("Ansible")  # 1
+print(f"Count of 'Ansible': {count_ansible}")   
+
+# k8s_idx = sample_tuple.index("k8s")  # This will raise an error because 'k8s' is not in the tuple (case-sensitive)
+"""
+Traceback (most recent call last):
+  File "C:\devops\bhupathi4245\repos\devops-python\03_tuples.py", line 37, in <module>
+    k8s_idx = sample_tuple.index("k8s")  # This will raise an error because 'k8s' is not in the tuple (case-sensitive)
+ValueError: tuple.index(x): x not found """
+
+# Tuple unpacking
+tool1, tool2, tool3, tool4, tool5 = sample_tuple
+print(f"Unpacked tools: {tool1}, {tool2}, {tool3}, {tool4}, {tool5}")
+
 # Method 2: Using the tuple() constructor
